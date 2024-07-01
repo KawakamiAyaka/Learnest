07330ab039d94b675578038d448fdf49476cac11
川上采華 <2230024@ecc.ac.jp>
2024-07-01 16:55:58 +0900

    投稿画面の途中 色変えそう

--- src/Components/PostBox.tsx
+++ src/Components/PostBox.tsx
@@ -35,7 +35,48 @@ export default function PostBox(){
                         />
                     </div>
                 </div>
+                <div className={style.PostContent}>
+                    <Image
+                    className={style.designImg}
+                    src="/iceCreamLogo.png"
+                    width={500}
+                    height={350}
+                    alt="作品のサムネ"
+                    />
+                    <div className={style.contentBox}>
+                        <h2 className={style.Text}>「Melty」という名の架空のアイスクリーム屋のロゴをデザイン。Meltyは溶けている状態を表す言葉。元気な子供がたくさん来店する店。</h2>
+                        <div className={style.pointBox}>
+                            <div className={style.pointTitle}>
+                                <p>Point</p>
+                                <Image
+                                className={style.pointImg}
+                                src="/light.svg"
+                                width={20}
+                                height={20}
+                                alt="Pointの電球マーク"
+                                    />
+                            </div>
+                            <p className={style.pointText}>溶けている状態を表現するため、アイスや背景に溶けかけている雫をつけました。また、ターゲット層が元気な子供なのでパワフルを表現するため原色の色を使いカラフルにしました。</p>
+                        </div>
+                    </div>
+                </div>
+                <div className={style.contentBtm}>
+                    <div className={style.User}>
+                        <Image
+                        className={style.userIcon}
+                        src="/iconOne.svg"
+                        width={60}
+                        height={60}
+                        alt="ユーザーアイコン"
+                        />
+                        <p>Yamada Hana</p>
+                        {/* ここにランクアイコン 下記仮の図形*/}
+                        <div className={style.rankIcon}>
 
+                        </div>
+                    </div>
+                    <button className={style.feedbackBtn}>フィードバックする</button>
+                </div>
             </div>
         </>
     )
