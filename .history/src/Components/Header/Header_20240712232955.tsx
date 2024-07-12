eebbbb5c1ccab5ebf23bb58af9df923ad9bbd6c7
川上采華 <2230024@ecc.ac.jp>
2024-07-12 23:52:35 +0900

    header書き換えバージョン2

---
+++ .history/src/Components/Header/Header_20240712232955.tsx
@@ -0,0 +1,87 @@
+// 'use client';
+import Image from 'next/image'
+import style from "@/Components/Header/Header.module.scss"
+import Link from 'next/link';
+
+export default function Header(){
+    return(
+        <>
+            <header className={style.header}>
+                <Image
+                style={{marginLeft:'20px'}}
+                src="LearnestLogo.svg"
+                width={250}
+                height={96}
+                alt="larnestのロゴ"
+                />
+                <nav>
+                    <ul style={{
+                            display:'flex',
+                            listStyleType:'none'
+                        }}>
+                        <li style={{
+                                margin:'0 10px',
+                                width:'8rem',
+                                height:'2.5rem',
+                                textAlign:'center',
+                                lineHeight:'2.5rem'
+                            }}>
+                            <Link href="/" 
+                                style={{
+                                    position:'relative',
+                                    textDecoration:'none',
+                                    fontWeight:'bold'
+                                }}
+                                className={style.Link}>トップ</Link>
+                        </li>
+                        <li style={{
+                                margin:'0 10px',
+                                width:'8rem',
+                                height:'2.5rem',
+                                textAlign:'center',
+                                lineHeight:'2.5rem'
+                            }}>
+                            <Link href="/themaPost" 
+                                style={{
+                                    position:'relative',
+                                    textDecoration:'none',
+                                    fontWeight:'bold'
+                                }} 
+                                className={style.Link}>お題を見る</Link>
+                        </li>
+                        <li style={{
+                                margin:'0 10px',
+                                width:'8rem',
+                                height:'2.5rem',
+                                textAlign:'center',
+                                lineHeight:'2.5rem'
+                            }}>
+                            <Link href="/inputPost" 
+                                style={{
+                                    position:'relative',
+                                    textDecoration:'none',
+                                    fontWeight:'bold'
+                                }} 
+                                className={style.Link}>投稿する</Link>
+                        </li>
+                        <li style={{
+                                margin:'0 10px',
+                                width:'8rem',
+                                height:'2.5rem',
+                                textAlign:'center',
+                                lineHeight:'2.5rem'
+                            }}>
+                            <Link href="/profile" 
+                                style={{
+                                    position:'relative',
+                                    textDecoration:'none',
+                                    fontWeight:'bold'
+                                }}
+                                className={style.Link}>プロフィール</Link>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
+        </>
+    )
+}
