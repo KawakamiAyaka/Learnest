fd16c670d3bf0c66d8649b44aea21c2b130cbc86
川上采華 <2230024@ecc.ac.jp>
2024-07-19 14:09:09 +0900

    feedback画面一時完成

--- src/Components/Star/Star.tsx
+++ src/Components/Star/Star.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useState } from 'react';
 import style from "@/Components/Star/Star.module.scss"
+import Link from 'next/link';
 
 export default function InputPost() {
     const [designRating, setDesignRating] = useState(0);
@@ -16,79 +17,82 @@ export default function InputPost() {
     return (
         <>
             <div className={style.main}>
-                <h2 className={style.h2}>[ 評価アンケート ]</h2>
-                    <h3>1. デザイン全体評価</h3>
-                    {[1, 2, 3, 4, 5].map((value) => (
-                        <button
-                            className={style.StarBtn}
-                            key={value}
-                            onClick={() => handleRating(setDesignRating, value)}
-                            style={{
-                                fontSize: '30px',
-                                color: value <= designRating ? 'gold' : 'lightgray',
-                                cursor: 'pointer',
-                            }}
-                        >
-                            ★
-                        </button>
-                    ))}
+                <div>
+                    <h2 className={style.h2}>[ 評価アンケート ]</h2>
+                        <h3>1. デザイン全体評価</h3>
+                        {[1, 2, 3, 4, 5].map((value) => (
+                            <button
+                                className={style.StarBtn}
+                                key={value}
+                                onClick={() => handleRating(setDesignRating, value)}
+                                style={{
+                                    fontSize: '30px',
+                                    color: value <= designRating ? 'gold' : 'lightgray',
+                                    cursor: 'pointer',
+                                }}
+                            >
+                                ★
+                            </button>
+                        ))}
 
-                    <h3>2. 色についての評価</h3>
-                    {[1, 2, 3].map((value) => (
-                        <button
-                            className={style.StarBtn}
-                            key={value}
-                            onClick={() => handleRating(setColorRating, value)}
-                            style={{
-                                fontSize: '30px',
-                                color: value <= colorRating ? 'gold' : 'lightgray',
-                                cursor: 'pointer',
-                            }}
-                        >
-                            ★
-                        </button>
-                    ))}
+                        <h3>2. 色についての評価</h3>
+                        {[1, 2, 3].map((value) => (
+                            <button
+                                className={style.StarBtn}
+                                key={value}
+                                onClick={() => handleRating(setColorRating, value)}
+                                style={{
+                                    fontSize: '30px',
+                                    color: value <= colorRating ? 'gold' : 'lightgray',
+                                    cursor: 'pointer',
+                                }}
+                            >
+                                ★
+                            </button>
+                        ))}
 
-                    <h3>3. 形やレイアウトの評価</h3>
-                    {[1, 2, 3].map((value) => (
-                        <button
-                            className={style.StarBtn}
-                            key={value}
-                            onClick={() => handleRating(setLayoutRating, value)}
-                            style={{
-                                fontSize: '30px',
-                                color: value <= layoutRating ? 'gold' : 'lightgray',
-                                cursor: 'pointer',
-                            }}
-                        >
-                            ★
-                        </button>
-                    ))}
+                        <h3>3. 形やレイアウトの評価</h3>
+                        {[1, 2, 3].map((value) => (
+                            <button
+                                className={style.StarBtn}
+                                key={value}
+                                onClick={() => handleRating(setLayoutRating, value)}
+                                style={{
+                                    fontSize: '30px',
+                                    color: value <= layoutRating ? 'gold' : 'lightgray',
+                                    cursor: 'pointer',
+                                }}
+                            >
+                                ★
+                            </button>
+                        ))}
 
-                    <h3>4. 創造性の評価</h3>
-                    {[1, 2, 3].map((value) => (
-                        <button
-                            className={style.StarBtn}
-                            key={value}
-                            onClick={() => handleRating(setCreativityRating, value)}
-                            style={{
-                                fontSize: '30px',
-                                color: value <= creativityRating ? 'gold' : 'lightgray',
-                                cursor: 'pointer',
-                            }}
-                        >
-                            ★
-                        </button>
-                    ))}
-                    <h3>感想、気づいたこと、アドバイス等</h3>
-                    <textarea
-                        className={style.text}
-                        value={feedback}
-                        onChange={(e) => setFeedback(e.target.value)}
-                        rows={5}
-                        cols={50}
-                        placeholder="ここに入力してください"
-                    ></textarea>
+                        <h3>4. 創造性の評価</h3>
+                        {[1, 2, 3].map((value) => (
+                            <button
+                                className={style.StarBtn}
+                                key={value}
+                                onClick={() => handleRating(setCreativityRating, value)}
+                                style={{
+                                    fontSize: '30px',
+                                    color: value <= creativityRating ? 'gold' : 'lightgray',
+                                    cursor: 'pointer',
+                                }}
+                            >
+                                ★
+                            </button>
+                        ))}
+                        <h3>感想、気づいたこと、アドバイス等</h3>
+                        <textarea
+                            className={style.text}
+                            value={feedback}
+                            onChange={(e) => setFeedback(e.target.value)}
+                            rows={5}
+                            cols={50}
+                            placeholder="ここに入力してください"
+                        ></textarea>
+                    </div>
+                    <Link className={style.link} href="/">送信</Link>
             </div>
         </>
     );
--- src/app/feedback/page.tsx
+++ src/app/feedback/page.tsx
@@ -8,6 +8,7 @@ export default function FeedbackForm() {
             <main className={style.main}>
                 <Canvas />
                 <StarComponents />
+            
             </main>
         </>
     )
