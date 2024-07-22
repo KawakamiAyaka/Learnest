e78cc0ea93a6845897af931a184d965e9e84eca3
川上采華 <2230024@ecc.ac.jp>
2024-07-22 16:15:48 +0900

    プロフィール画面基礎

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,7 +1,30 @@
+import style from "@/app/profile/profile.module.scss"
+import ItemWrap from "@/Components/ItemWrap/ItemWrap"
+import Link from "next/link"
+
 
 export default function profile() {
     return (
         <>
+            <main className={style.main}>
+                <div className={style.markWrap}>
+                    <ul className={style.nav}>
+                        <li><Link href="#">保存済み</Link></li>
+                        <li><Link href="#">投稿アーカイブ</Link></li>
+                        <li><Link href="#">フィードバックアーカイブ</Link></li>
+                    </ul>
+                <div className={style.marlBox}>
+                    <ItemWrap/>
+                    <ItemWrap/>
+                    <ItemWrap/>
+                    <ItemWrap/>
+                    <ItemWrap/>
+                </div>
+                </div>
+                <div className={style.profile}>
+                    
+                </div>
+            </main>
         </>
     )
 }
